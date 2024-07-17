8a681f2c63a09c6968b2f3fc4a855c627e579885
Shahryar <shahryarmashouf.1998@gmail.com>
2024-07-17 13:02:11 +0430

    n2

--- deutschwithusapi/models.py
+++ deutschwithusapi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    level = models.CharField(max_length=50)
+    time = models.TimeField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    def __str__(self):
+        return self.name
--- deutschwithusapi/views.py
+++ deutschwithusapi/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .serializers import CourseSerializer
+from .models import Course
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import JsonResponse
+from rest_framework import status
 
-# Create your views here.
+
+
+@api_view(['GET'])
+def courses(request):
+    items = Course.objects.all()  
+    serializer = CourseSerializer(items, many=True) 
+    return Response({'course':serializer.data})
+
+@api_view(['POST'])
+def courseCreate(request):
+    serializer = CourseSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+    
+    return Response(serializer.data)
